21c74a7b594700363c224491acf04591feb72ac7
ArtemBabaiev <artdcxx4@gmail.com>
2025-05-24 19:55:25 +0300

    Updated 2 files

--- scripts/create-company.sh
+++ scripts/create-company.sh
@@ -1,3 +1,7 @@
 aws --endpoint-url=http://localhost:9324 sqs send-message \
   --queue-url http://localhost:9324/queue/main-queue \
-  --message-body '{"eventType": "create.organization", "name": "Test Org", "description": "desc"}'
+  --message-body '{
+    "eventType": "create.organization",
+    "name": "Test Org",
+    "description": "desc"
+}'
